f96d371e0ef5beb25e7eec84b311c6e3d106d58d
Erick Lugo <ericklugoj@gmail.com>
2021-02-26 21:40:07 -0600

    Arrays examples complete

--- Arrays/array.js
+++ Arrays/array.js
@@ -4,10 +4,12 @@ class MyArray {
     this.data = {};
   }
 
+  // Obtener un elemento del array por indice
   get(index) {
     return this.data[index];
   }
 
+  // Agregar un elemento al final del array
   push(item) {
     this.data[this.length] = item;
     this.length++;
@@ -15,7 +17,10 @@ class MyArray {
     return this.data;
   }
 
+  // Eliminar el ultimo elemento del array
   pop() {
+    if (this.length === 0) return [];
+
     const lastItem = this.data[this.length - 1];
     delete this.data[this.length - 1];
     this.length--;
@@ -23,6 +28,7 @@ class MyArray {
     return lastItem;
   }
 
+  // Eliminar un elemento del array por index
   delete(index) {
     const item = this.data[index];
     this.shiftIndex(index);
@@ -30,6 +36,7 @@ class MyArray {
     return item;
   }
 
+  // Re ordenar indice de arrays
   shiftIndex(index) {
     for (let i = index; i < this.length - 1; i++) {
       this.data[i] = this.data[i + 1];
@@ -37,6 +44,22 @@ class MyArray {
 
     delete this.data[this.length - 1];
     this.length--;
+  }
+
+  // Eliminar primer elemento del array
+  shift() {
+    return this.delete(0);
+  }
+
+  // Agregar un elemento al inicio
+  unshift(item) {
+    for (let i = this.length; i > 0; i--) {
+      this.data[i] = this.data[i - 1];
+    }
+    this.data[0] = item;
+    this.length++;
+
+    return this.length;
   }
 }
 
@@ -51,3 +74,13 @@ console.log(myArray.get(0));
 console.log(myArray.pop());
 
 console.log(myArray.delete(0));
+
+console.log(myArray.push('zeron'));
+console.log(myArray.push('didier'));
+console.log(myArray.push('beto'));
+
+console.log(myArray.shift());
+
+console.log(myArray.unshift('dayana'));
+
+console.log(myArray.data);
